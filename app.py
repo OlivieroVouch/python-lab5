a0b40ecc072bf989260aebe7b2d6350b11c6bbc0
identifier <name@github.com>
2018-04-29 00:52:12 +0200

    working

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask # import flask package
+from flask import render_template,url_for, request, redirect, session# function from Flask package
 
 app = Flask(__name__)
+import todo_database
 
+@app.route('/index')
+def index():
+    list = todo_database.showTask()
+    return render_template("index.html", result=list)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('index'))  # return the content of the page--> actually here we are not yet returning an HTML file but just a stringreturn redirect(url_for('index')) # return the content of the page--> actually here we are not yet returning an HTML file but just a string
 
 
 if __name__ == '__main__':
